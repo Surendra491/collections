f5312761d113034c981daef18f947948a486dd1d
Surendra419 <ysurendrareddy333@gmail.com>
2026-01-03 00:10:11 +0530

    constant time complexity

---
+++ src/main/java/com/basics/das/OrderOfOne.java
@@ -0,0 +1,11 @@
+package com.basics.das;
+
+public class OrderOfOne {
+
+    public static void main(String[] arg){
+        System.out.println(multiplyNumbers(124));
+    }
+    public static int multiplyNumbers(int n){
+        return n*n;
+    }
+}
