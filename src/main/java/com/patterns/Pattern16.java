fcf5f579da70ebd4e80083a49f140ebc495d455d
Surendra419 <ysurendrareddy333@gmail.com>
2026-01-18 23:57:40 +0530

    pattern 16

--- src/main/java/com/patterns/Pattern16.java
+++ src/main/java/com/patterns/Pattern16.java
@@ -7,7 +7,7 @@ public class Pattern16 {
          //        1   1
          //      1   2   1
          //     1   3   3   1
-         //   1   4   6   4   1
+         //   1  4   6   4   1
 
     public static void main(String[] args){
         Pattern16 pattern16 = new Pattern16();
@@ -15,13 +15,15 @@ public class Pattern16 {
     }
 
     public void pattern16(int n) {
-        for (int row = 1; row <= n; row++) {
+        for (int row = 0; row < n; row++) {
 
+            int c =1;
             for (int s = 0; s <n- row; s++) {
                 System.out.print(" ");
             }
-           for (int i = 1; i < row; i++ ){
-               System.out.print(i);
+           for (int i = 0; i <= row; i++ ){
+               System.out.print(c+" ");
+               c = c*(row - i)/(i +1);
            }
 
             System.out.println();
