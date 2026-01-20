c0970a71e6db5cc01d581599e4b775a8bb2f7c88
Surendra419 <ysurendrareddy333@gmail.com>
2026-01-21 01:39:51 +0530

    pattern 20

---
+++ src/main/java/com/patterns/Pattern20.java
@@ -0,0 +1,35 @@
+package com.patterns;
+
+public class Pattern20 {
+
+/*
+
+       ****
+       *  *
+       *  *
+       *  *
+       ****
+
+*/
+
+    public static void main(String[] args){
+        Pattern20 pattern20 = new Pattern20();
+        pattern20.pattern20(5);
+    }
+
+    public void pattern20(int n) {
+        for (int row = 0; row < n ; row++) {
+            for (int col =0; col <n-1; col++){
+                if (row == 0 || row ==n-1 || col ==0 || col == n-2) {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+
+
+            }
+            System.out.println();
+        }
+    }
+}
+
