c0ddb7b19838f7eccde83458663e43be3e79b9fd
Surendra419 <ysurendrareddy333@gmail.com>
2026-03-07 00:19:18 +0530

    Pattern23 need to revisit again this commit is for test git deafult editor

--- src/main/java/com/patterns/Pattern23.java
+++ src/main/java/com/patterns/Pattern23.java
@@ -4,20 +4,18 @@ public class Pattern23 {
 
 /*
 
-       ****
-       *  *
-       *  *
-       *  *
-       ****
+        *      *
+      *   *  *   *
+    *      *      *
 
 */
 
     public static void main(String[] args){
-        Pattern23 pattern20 = new Pattern23();
-        pattern20.pattern20(5);
+        Pattern23 pattern23 = new Pattern23();
+        pattern23.pattern23(5);
     }
 
-    public void pattern20(int n) {
+    public void pattern23(int n) {
         for (int row = 0; row < n ; row++) {
             for (int col =0; col <n-1; col++){
                 if (row == 0 || row ==n-1 || col ==0 || col == n-2) {
